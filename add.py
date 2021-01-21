2575e688863984f75fdaa3409c6f66d5db06fc50
Yeswanthreddy22 <77447278+Yeswanthreddy22@users.noreply.github.com>
2021-01-21 11:36:04 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,107 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "20\n"
+     ]
+    }
+   ],
+   "source": [
+    "num1=10\n",
+    "num2=10\n",
+    "sum=num1+num2\n",
+    "print(sum)\n"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 3,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "sum of 10 and 20 is 30\n"
+     ]
+    }
+   ],
+   "source": [
+    "num1=10\n",
+    "num2=20\n",
+    "sum=num1+num2\n",
+    "print(\"sum of {0} and {1} is {2}\".format(num1,num2,sum))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdin",
+     "output_type": "stream",
+     "text": [
+      "first number 0.25\n",
+      "secod number 0.35\n"
+     ]
+    },
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "sum of 0.25 and 0.35 is 0.6\n"
+     ]
+    }
+   ],
+   "source": [
+    "number1=input(\"first number\")\n",
+    "number2=input(\"secod number\")\n",
+    "sum=float(number1)+float(number2)\n",
+    "print(\"sum of {0} and {1} is {2}\".format(number1,number2,sum))"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.5"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 4
+}
