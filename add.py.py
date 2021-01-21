e35b61398ac360846946ebd169e6e4d839fccf80
Yeswanthreddy22 <77447278+Yeswanthreddy22@users.noreply.github.com>
2021-01-21 13:26:19 +0530

    Add files via upload

---
+++ add.py.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num1=10
+num2=20
+sum=num1+num2
+print(sum)
+
+
+# In[2]:
+
+
+num1 = 10
+num2 = 30
+sum  = num1+num2
+print("sum of {0} and {1} is {2}".format(num1,num2,sum))
+
+
+# In[3]:
+
+
+number1=input("first number")
+number2=input("secound number")
+sum = float(number1)+float(number2)
+print("sum of {0} and {1} is {2}".format(number1,number2,sum))
+
+
+# In[ ]:
+
+
+
+
